7aca07378a58449cb7d95b9bde36eed49345c67b
Victor Zhou <vzhou842@gmail.com>
2019-02-07 16:52:26 -0500

    Improved Sidebar styling for mobile

--- src/components/Sidebar/Author/Author.js
+++ src/components/Sidebar/Author/Author.js
@@ -4,18 +4,16 @@ import styles from './Author.module.scss';
 
 const Author = ({ author }) => (
   <div className={styles['author']}>
-    <Link to="/">
-      <img
-        src={withPrefix(author.photo)}
-        className={styles['author__photo']}
-        width="75"
-        height="75"
-        alt={author.name}
-      />
-    </Link>
-    <h2 className={styles['author__title']}>
-      <Link className={styles['author__title-link']} to="/">{author.name}</Link>
-    </h2>
+    <div className={styles['author__main-section']}>
+      <Link to="/">
+        <img src={withPrefix(author.photo)} className={styles['author__photo']} alt={author.name} />
+      </Link>
+      <h2 className={styles['author__title']}>
+        <Link className={styles['author__title-link']} to="/">
+          {author.name}
+        </Link>
+      </h2>
+    </div>
     <p className={styles['author__subtitle']}>{author.bio}</p>
   </div>
 );
