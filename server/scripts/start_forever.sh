1d0a04b1f41ec32253510966abd117f2c11cb71e
Victor Zhou <vzhou842@gmail.com>
2019-02-09 15:37:21 -0500

    Fix start_forever.sh

--- server/scripts/start_forever.sh
+++ server/scripts/start_forever.sh
@@ -1,7 +1,7 @@
 function start() {
 	sudo NODE_ENV='production' \
 	PORT=$1 \
-	forever --uid "web-$1" -o $2 -e $3 start app.js
+	forever --uid "web-$1" -o $2 -e $3 --append start server/server.js
 }
 
 start 8000 ./out1.log ./err1.log
