aad8e5e3f97514ede02ff625abe720ed4c750990
Victor Zhou <vzhou842@gmail.com>
2019-05-03 15:10:55 -0400

    Replace root Content element with <article>

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -3,12 +3,12 @@ import moment from 'moment';
 import styles from './Content.module.scss';
 
 const Content = ({ body, title, subtitle, date }) => (
-  <div className={styles['content']}>
+  <article className={styles['content']}>
     <h1 className={styles['content__title']}>{title}</h1>
     <h2 className={styles['content__subtitle']}>{subtitle}</h2>
     <p className={styles['content__date']}><time>{moment(date).format('MMMM DD, YYYY')}</time></p>
     <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
-  </div>
+  </article>
 );
 
 export default Content;
