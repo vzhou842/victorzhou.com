ab1fabc967b1593279eae578f1367e877a72beb6
Victor Zhou <vzhou842@gmail.com>
2019-06-15 22:58:14 +0800

    Add tag description for "Machine Learning"

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -12,13 +12,39 @@ type Props = {|
   +pageContext: Object,
 |};
 
-const tagDescriptions = {};
+const tagDescriptions = {
+  'Machine Learning': (
+    <div>
+      <p>
+        Machine Learning is, put simply, getting computers to generalize from examples. And that's
+        what I try to do: <b>put [seemingly complicated] things simply</b>. My posts on Machine
+        Learning (ML) consist primarily of beginner-focused introductions to common ML models or
+        concepts. I felt like too many ML tutorials weren't accessible enough, so I strove to make
+        my guides <b>as clear and beginner-friendly as possible</b>.
+      </p>
+      <p style={{ marginBottom: 0 }}>
+        Unsure where to start? Here are some of my best / most popular posts:
+      </p>
+      <ul>
+        <li>
+          <a href="/blog/intro-to-neural-networks/">
+            Machine Learning for Beginners: An Introduction to Neural Networks
+          </a>
+        </li>
+        <li>
+          <a href="/blog/intro-to-cnns-part-1/">An Introduction to Convolutional Neural Networks</a>
+        </li>
+        <li>
+          <a href="/blog/intro-to-random-forests/">Random Forests for Complete Beginners</a>
+        </li>
+      </ul>
+      <p>Happy Reading!</p>
+    </div>
+  ),
+};
 
 const TagTemplate = ({ data, pageContext }: Props) => {
-  const {
-    title: siteTitle,
-    subtitle: siteSubtitle
-  } = data.site.siteMetadata;
+  const { title: siteTitle, subtitle: siteSubtitle } = data.site.siteMetadata;
 
   const {
     tag,
@@ -31,12 +57,19 @@ const TagTemplate = ({ data, pageContext }: Props) => {
   } = pageContext;
 
   const { edges } = data.allMarkdownRemark;
-  const pageTitle = currentPage > 0 ? `All Posts tagged as "${tag}" - Page ${currentPage} - ${siteTitle}` : `All Posts tagged as "${tag}" - ${siteTitle}`;
+  const pageTitle =
+    currentPage > 0 ?
+      `All Posts tagged as "${tag}" - Page ${currentPage} - ${siteTitle}` :
+      `All Posts tagged as "${tag}" - ${siteTitle}`;
 
   return (
     <Layout title={pageTitle} description={siteSubtitle}>
       <Sidebar />
-      <Page title={tag} subtitle={<a href="/tags/">← Back to All Tags</a>} description={tagDescriptions[tag]}>
+      <Page
+        title={tag}
+        subtitle={<a href="/tags/">← Back to All Tags</a>}
+        description={tagDescriptions[tag]}
+      >
         <Feed edges={edges} />
         <Pagination
           currentPage={currentPage}
@@ -60,11 +93,13 @@ export const query = graphql`
       }
     }
     allMarkdownRemark(
-        limit: $postsLimit,
-        skip: $postsOffset,
-        filter: { frontmatter: { tags: { in: [$tag] }, template: { eq: "post" }, draft: { ne: true } } },
-        sort: { order: DESC, fields: [frontmatter___date] }
-      ){
+      limit: $postsLimit
+      skip: $postsOffset
+      filter: {
+        frontmatter: { tags: { in: [$tag] }, template: { eq: "post" }, draft: { ne: true } }
+      }
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
       edges {
         node {
           fields {
