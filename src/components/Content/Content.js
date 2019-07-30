cf2a6b2451022661261e819d10ac865d4376f20b
Victor Zhou <vzhou842@gmail.com>
2019-07-29 22:51:11 -0400

    Add Flow typing to <Content />

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,8 +1,17 @@
+// @flow
 import React from 'react';
 import styles from './Content.module.scss';
 import ContentDate from '../ContentDate';
 
-const Content = ({ body, title, subtitle, date, dateModified }) => (
+type Props = {|
+  +body: string,
+  +title: string,
+  +subtitle: string,
+  +date: Date,
+  +dateModified: ?Date,
+|};
+
+const Content = ({ body, title, subtitle, date, dateModified }: Props) => (
   <article className={styles['content']}>
     <h1 className={styles['content__title']}>{title}</h1>
     <h2 className={styles['content__subtitle']}>{subtitle}</h2>
