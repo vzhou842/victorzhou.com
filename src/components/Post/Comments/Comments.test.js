24b32bc7574d3f1b29fadb623a1ac460c4e7dcdc
Victor Zhou <vzhou842@gmail.com>
2019-11-03 23:50:19 -0800

    Fix Comments test

--- src/components/Post/Comments/Comments.test.js
+++ src/components/Post/Comments/Comments.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { Comments } from './Comments';
+import Comments from './Comments';
 
 describe('Comments', () => {
   it('renders correctly', () => {
