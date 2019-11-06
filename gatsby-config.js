4507d995aad7abe7ac9dbadc8377dc63a4c3a8d0
Victor Zhou <vzhou842@gmail.com>
2019-11-05 22:06:13 -0800

    Set gatsby-plugin-sharp defaultQuality to 90

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,7 +125,12 @@ module.exports = {
       }
     },
     'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-sharp',
+      options: {
+        defaultQuality: 90,
+      },
+    },
     {
       resolve: 'gatsby-plugin-google-gtag',
       options: {
