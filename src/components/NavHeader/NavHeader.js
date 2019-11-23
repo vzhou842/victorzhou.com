4c0a4c9bcd4831f50b5f960a4fb6163652b9667b
Victor Zhou <vzhou842@gmail.com>
2019-11-23 00:54:58 -0800

    Integrate react-headroom

--- src/components/NavHeader/NavHeader.js
+++ src/components/NavHeader/NavHeader.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import Headroom from 'react-headroom';
 import styles from './NavHeader.module.scss';
 
 import Author from '../Author';
 import Menu from '../Menu';
 
 const NavHeader = () => (
-  <div className={styles['header']}>
-    <div className={styles['header__left']}>
-      <Author />
+  <Headroom>
+    <div className={styles['header']}>
+      <div className={styles['header__left']}>
+        <Author />
+      </div>
+      <div className={styles['header__right']}>
+        <Menu horizontal bold />
+      </div>
     </div>
-    <div className={styles['header__right']}>
-      <Menu horizontal bold />
-    </div>
-  </div>
+  </Headroom>
 );
 
 export default NavHeader;
