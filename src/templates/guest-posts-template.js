92a8113ec36fd8ab109440354e340313c71173ab
Victor Zhou <vzhou842@gmail.com>
2020-01-09 23:30:53 -0800

    Update More Posts header section

--- src/templates/guest-posts-template.js
+++ src/templates/guest-posts-template.js
@@ -23,13 +23,31 @@ const GuestPostsTemplate = ({ data }: Props) => {
     <TemplateWrapper>
       <Layout title={pageTitle} description={siteSubtitle}>
         <Sidebar />
-        <Page title="More Posts">
-          <p>
-            While posts in my <Link to="/">Blog</Link> section are all written by me, posts in this
-            section were written by guest authors. Even if I didn't directly contribute to the
-            writing of the post, <b>I personally review and edit every guest post I publish</b>,
-            meaning I believe these are all quality articles!
-          </p>
+        <Page
+          title="More Posts"
+          description={
+            <>
+              <p style={{ marginBottom: 0 }}>
+                While posts in my <Link to="/">Blog</Link> section are all written by me, posts in
+                this section were written by either:
+              </p>
+              <ul style={{ marginBottom: 0 }}>
+                <li>
+                  <b>Co-Authors</b>, who helped me write the post, or
+                </li>
+                <li>
+                  <b>Guest Authors</b>, who wrote the post independently.
+                </li>
+              </ul>
+              <p>
+                I still <b>personally review and edit every guest post I publish</b>, meaning I
+                believe these are all quality articles! If you're interested in guesting posting or
+                co-authoring a post with me, feel free to{' '}
+                <Link to="/about/#contact-me">get in touch</Link>.
+              </p>
+            </>
+          }
+        >
           <Feed edges={edges} />
         </Page>
       </Layout>
