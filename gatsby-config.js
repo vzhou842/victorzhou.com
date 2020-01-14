d9b8f890cb5dbf9ef8c8bf42b28f4c687ab07b15
Victor Zhou <victorzhou@fb.com>
2020-01-13 18:49:34 -0700

    Fix lint

--- gatsby-config.js
+++ gatsby-config.js
@@ -194,22 +194,22 @@ module.exports = {
               // Use cacheFirst since these don't need to be revalidated (same RegExp
               // and same reason as above)
               urlPattern: /(\.js$|\.css$|static\/)/,
-              handler: `cacheFirst`,
+              handler: 'cacheFirst',
             },
             {
               // page-data.json files are not content hashed
               urlPattern: /^https?:.*\page-data\/.*\/page-data\.json/,
-              handler: `networkFirst`,
+              handler: 'networkFirst',
             },
             {
               // Add runtime caching of various other page resources
               urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
-              handler: `staleWhileRevalidate`,
+              handler: 'staleWhileRevalidate',
             },
             {
               // Google Fonts CSS (doesn't end in .css so we need to specify it)
               urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
-              handler: `staleWhileRevalidate`,
+              handler: 'staleWhileRevalidate',
             },
             // Everything above is from the default config.
             {
