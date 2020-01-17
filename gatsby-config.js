9ea711de4e43baa43634c92e6d4547f9b30c78fc
Victor Zhou <vzhou842@gmail.com>
2020-01-17 00:11:18 -0800

    Update workbox runtimeCaching handlers to fix build warnings

--- gatsby-config.js
+++ gatsby-config.js
@@ -211,22 +211,22 @@ module.exports = {
               // Use cacheFirst since these don't need to be revalidated (same RegExp
               // and same reason as above)
               urlPattern: /(\.js$|\.css$|static\/)/,
-              handler: 'cacheFirst',
+              handler: 'CacheFirst',
             },
             {
               // page-data.json files are not content hashed
               urlPattern: /^https?:.*\page-data\/.*\/page-data\.json/,
-              handler: 'networkFirst',
+              handler: 'NetworkFirst',
             },
             {
               // Add runtime caching of various other page resources
               urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
-              handler: 'staleWhileRevalidate',
+              handler: 'StaleWhileRevalidate',
             },
             {
               // Google Fonts CSS (doesn't end in .css so we need to specify it)
               urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
-              handler: 'staleWhileRevalidate',
+              handler: 'StaleWhileRevalidate',
             },
             // Everything above is from the default config.
             {
@@ -234,7 +234,7 @@ module.exports = {
               // Does this actually work? idk.
               // Why doesn't the root page always hit the network? Also idk.
               urlPattern: new RegExp(`${siteConfig.url}/$`),
-              handler: 'networkFirst',
+              handler: 'NetworkFirst',
             },
           ],
         },
