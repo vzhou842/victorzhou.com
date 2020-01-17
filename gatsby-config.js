6ba8686848be029250b0a64c6b8aed870ace61bc
Victor Zhou <vzhou842@gmail.com>
2020-01-16 23:14:34 -0800

    Filter out guest posts from RSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,13 @@ module.exports = {
                 allMarkdownRemark(
                   limit: 1000,
                   sort: { order: DESC, fields: [frontmatter___date] },
-                  filter: { frontmatter: { template: { eq: "post" }, draft: { ne: true } } }
+                  filter: {
+                    frontmatter: {
+                      template: { eq: "post" },
+                      draft: { ne: true },
+                      guestAuthor: { in: [null, ""] },
+                    }
+                  }
                 ) {
                   edges {
                     node {
