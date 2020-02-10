f62bdfe604fa1d52f32ebde792596b6fb0ad8764
Victor Zhou <victorzhou@fb.com>
2020-02-09 20:54:27 -0800

    Implement mobile hamburger

--- src/components/NavHeader/NavHeader.js
+++ src/components/NavHeader/NavHeader.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames/bind';
 import Headroom from 'react-headroom';
 import styles from './NavHeader.module.scss';
 
 import Author from '../Author';
+import PlatformContext from '../PlatformContext';
 import Menu from '../Menu';
 
-const NavHeader = () => (
-  <Headroom>
-    <div className={styles['header']}>
-      <div className={styles['header__left']}>
-        <Author />
-      </div>
-      <div className={styles['header__right']}>
-        <Menu horizontal bold />
-      </div>
-    </div>
-  </Headroom>
-);
+const cx = classNames.bind(styles);
+
+function NavHeader() {
+  const [menuShown, setMenuShown] = useState(false);
+
+  return (
+    <PlatformContext render={isMobile => (
+      <Headroom onUnpin={() => { setMenuShown(false); }}>
+        <div className={cx({ header: true, 'no-shadow': menuShown })}>
+          <div className={styles['header__left']}>
+            <Author />
+          </div>
+          {isMobile ? (
+            <button
+              onClick={() => { setMenuShown(!menuShown); }}
+              className={cx({ header__burger: true, open: menuShown })}>
+              <img src="/menu.svg" width={28} height={28} />
+            </button>
+          ) : (
+            <div className={styles['header__right']}>
+              <Menu horizontal bold />
+            </div>
+          )}
+        </div>
+        {isMobile && menuShown && (
+          <div className={styles['popup']}>
+            <Menu bold noMargin />
+          </div>
+        )}
+      </Headroom>
+    )} />
+  );
+}
 
 export default NavHeader;
