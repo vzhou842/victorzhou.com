7c81ecfa57cb7de208b29ab7badbc1dfc552a036
Victor Zhou <victorzhou@fb.com>
2020-06-07 20:24:50 -0700

    Implement Share Icon fade in on scroll

--- src/components/ShareIcons/ShareIcons.js
+++ src/components/ShareIcons/ShareIcons.js
@@ -1,6 +1,7 @@
 // @flow
+import classnames from 'classnames/bind';
 import { graphql, StaticQuery } from 'gatsby';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   EmailIcon,
   EmailShareButton,
@@ -19,6 +20,8 @@ import {
 import { logEvent } from '../../utils/log';
 import styles from './ShareIcons.module.scss';
 
+const cx = classnames.bind(styles);
+
 type Props = {
   +title: string,
   +url: string,
@@ -28,45 +31,76 @@ type PureProps = {
   +twitter: string,
 } & Props;
 
-const PureShareIcons = ({ title, url, twitter }: PureProps) => (
-  <div className={styles['share']}>
-    <EmailShareButton beforeOnClick={() => logEvent('ShareIcon', 'email-click')} subject={title} url={url}>
-      <EmailIcon size={32} />
-    </EmailShareButton>
-    <FacebookShareButton beforeOnClick={() => logEvent('ShareIcon', 'facebook-click')} url={url}>
-      <FacebookIcon size={32} />
-    </FacebookShareButton>
-    <TwitterShareButton
-      beforeOnClick={() => logEvent('ShareIcon', 'twitter-click')}
-      url={url}
-      title={title}
-      via={twitter}
-    >
-      <TwitterIcon size={32} />
-    </TwitterShareButton>
-    <LinkedinShareButton
-      beforeOnClick={() => logEvent('ShareIcon', 'linkedin-click')}
-      url={url}
-      title={title}
-    >
-      <LinkedinIcon size={32} />
-    </LinkedinShareButton>
-    <RedditShareButton
-      beforeOnClick={() => logEvent('ShareIcon', 'reddit-click')}
-      url={url}
-      title={title}
-    >
-      <RedditIcon size={32} bgStyle={{ fill: 'ff4500' }} />
-    </RedditShareButton>
-    <WhatsappShareButton
-      beforeOnClick={() => logEvent('ShareIcon', 'whatsapp-click')}
-      url={url}
-      title={title}
-    >
-      <WhatsappIcon size={32} />
-    </WhatsappShareButton>
-  </div>
-);
+function PureShareIcons({ title, url, twitter }: PureProps) {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (visible) {
+      return;
+    }
+
+    const threshold = Math.min(
+      Math.max((document.body ? document.body.offsetHeight : 0) / 8, 500),
+      1250
+    );
+
+    const scrollListener = () => {
+      if (window.scrollY + window.innerHeight / 2 >= threshold) {
+        setVisible(true);
+      }
+    };
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    };
+  }, [visible, setVisible]);
+
+  return (
+    <div className={cx({ share: true, hidden: !visible })}>
+      <EmailShareButton
+        beforeOnClick={() => logEvent('ShareIcon', 'email-click')}
+        subject={title}
+        url={url}
+      >
+        <EmailIcon size={32} />
+      </EmailShareButton>
+      <FacebookShareButton beforeOnClick={() => logEvent('ShareIcon', 'facebook-click')} url={url}>
+        <FacebookIcon size={32} />
+      </FacebookShareButton>
+      <TwitterShareButton
+        beforeOnClick={() => logEvent('ShareIcon', 'twitter-click')}
+        url={url}
+        title={title}
+        via={twitter}
+      >
+        <TwitterIcon size={32} />
+      </TwitterShareButton>
+      <LinkedinShareButton
+        beforeOnClick={() => logEvent('ShareIcon', 'linkedin-click')}
+        url={url}
+        title={title}
+      >
+        <LinkedinIcon size={32} />
+      </LinkedinShareButton>
+      <RedditShareButton
+        beforeOnClick={() => logEvent('ShareIcon', 'reddit-click')}
+        url={url}
+        title={title}
+      >
+        <RedditIcon size={32} bgStyle={{ fill: 'ff4500' }} />
+      </RedditShareButton>
+      <WhatsappShareButton
+        beforeOnClick={() => logEvent('ShareIcon', 'whatsapp-click')}
+        url={url}
+        title={title}
+      >
+        <WhatsappIcon size={32} />
+      </WhatsappShareButton>
+    </div>
+  );
+}
 
 export const ShareIcons = ({ url, ...props }: Props) => (
   <StaticQuery
